0f3405f1c0e44c5df83eaf6b64830bc958b42a4c
Eric Kim <erickim195@gmail.com>
2022-01-05 15:43:24 -0800

    Set up base server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import cors from 'cors'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
 
 export const app = express()
 const PORT = '3000'
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
     res.send("Hello World")
