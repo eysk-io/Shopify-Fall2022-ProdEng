8581935396232c95d93dde80ca88f5ce8b814e1e
Eric Kim <erickim195@gmail.com>
2022-01-05 20:23:21 -0800

    Add configs based on env

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+const env = process.env.NODE_ENV || 'development'
+
+let config = {
+    env,
+    port: process.env.PORT || 3000
+}
+
+switch (env) {
+    case 'prod':
+    case 'production':
+        config.dbUrl = process.env.MONGO_URI
+    case 'test':
+    case 'testing':
+        config.dbUrl = 'mongodb://localhost:27017/test'
+        break
+    case 'dev':
+    case 'development':
+    default:
+        config.dbUrl = 'mongodb://localhost:27017/dev'
+}
+
+export default config
