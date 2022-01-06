edc0fec0ada6c06579f761fdfbf09ad2898f10ef
Eric Kim <erickim195@gmail.com>
2022-01-05 20:27:34 -0800

    Add startServer and move server logic out of src/index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-import express from 'express'
-import cors from 'cors'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import path from 'path'
-import itemRouter from './resources/item/item.router'
-
-export const app = express()
-const PORT = '3000'
-
-app.disable('x-powered-by')
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
-})
-
-app.use('/api/item', itemRouter)
-
-app.listen(PORT, () => {
-    console.log(`Running on http://localhost:${PORT}`)
-})
+import { startServer } from './server'
+startServer()
---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import path from 'path'
+import itemRouter from './resources/item/item.router'
+
+export const app = express()
+const PORT = '3000'
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+app.use('/api/item', itemRouter)
+
+export const startServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Running on http://localhost:${PORT}`)
+        })
+    } catch (e) {
+        console.error(e)
+    }
+}
